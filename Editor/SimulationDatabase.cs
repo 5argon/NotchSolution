79ada1db78cd108ac11899cb9bb21a350242957d
EvanG <evan_g@orange.fr>
2019-02-09 21:26:16 +0100

    Two new devices: Huawei Mate 20 Pro & OnePlus 6T

--- Editor/SimulationDatabase.cs
+++ Editor/SimulationDatabase.cs
@@ -16,6 +16,18 @@ namespace E7.NotchSolution
                 landscapeSafeArea = new Rect(132, 63, 2172, 1062),
                 screenSize = new Vector2(1125, 2436),
             },
+            [SimulationDevice.OnePlus6T] = new SimulationDatabaseData
+            {
+                portraitSafeArea = new Rect(0, 79, 1080, 2261),
+                landscapeSafeArea = new Rect(79, 0, 2261, 1080),
+                screenSize = new Vector2(1080, 2340),
+            },
+            [SimulationDevice.HuaweiMate20Pro] = new SimulationDatabaseData
+            {
+                portraitSafeArea = new Rect(0, 100, 1440, 3020),
+                landscapeSafeArea = new Rect(100, 0, 3020, 1440),
+                screenSize = new Vector2(1440, 3120),
+            },
         };
     }
 }
