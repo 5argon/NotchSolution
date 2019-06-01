3072ecf7edbe57586bf47913ee89148a36369c4f
EvanG <evan_g@orange.fr>
2019-06-01 22:16:13 +0200

    Sorting devices by brand

--- Editor/SimulationDevice.cs
+++ Editor/SimulationDevice.cs
@@ -1,10 +1,12 @@
+using UnityEngine;
+
 namespace E7.NotchSolution
 {
     public enum SimulationDevice
     {
-        iPhoneX,
-        iPadPro,
-        OnePlus6T,
-        HuaweiMate20Pro,
+        [InspectorName("Apple/ iPhone X")] iPhoneX,
+        [InspectorName("Apple/ iPad Pro")] iPadPro,
+        [InspectorName("OnePlus/ 6T")] OnePlus6T,
+        [InspectorName("Huawei/ Mate 20 Pro")] HuaweiMate20Pro,
     }
 }
